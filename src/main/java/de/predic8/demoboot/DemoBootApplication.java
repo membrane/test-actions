f57d78871a02a7d2020d0b60e860ee7766ff248d
Thomas Bayer <bayer@predic8.de>
2023-02-06 19:21:56 +0100

    init

--- src/main/java/de/predic8/demoboot/DemoBootApplication.java
+++ src/main/java/de/predic8/demoboot/DemoBootApplication.java
@@ -3,10 +3,13 @@ package de.predic8.demoboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+
 @SpringBootApplication
 public class DemoBootApplication {
 
     public static void main(String[] args) {
+        System.out.println("args = " + Arrays.toString(args));
         SpringApplication.run(DemoBootApplication.class, args);
     }
 
