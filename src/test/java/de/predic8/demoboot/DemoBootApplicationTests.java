b88ad7c98f21ae0d7a5d0898eabdf45635158a65
Thomas Bayer <bayer@predic8.de>
2023-02-06 17:52:13 +0100

    init

---
+++ src/test/java/de/predic8/demoboot/DemoBootApplicationTests.java
@@ -0,0 +1,13 @@
+package de.predic8.demoboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
